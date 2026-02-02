84a8c2b2cdb393335cefaa43aaba8726494f7fb3
Sophie <sophie.thi.nguyen11@gmail.com>
2026-02-02 13:55:26 -0800

    refactor player controller code  and tweaks

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 1200.0
-const JUMP_VELOCITY = -1900.0
-const GRAVITY = 4000
-const coyote_frames = 6  # How many in-air frames to allow jumping
+@export var SPEED = 1400.0
+@export var JUMP_VELOCITY = -1900.0
+@export var GRAVITY = 4000
+@export var coyote_frames = 3  # How many in-air frames to allow jumping
+
 var coyote = false  # Track whether we're in coyote time or not
 var last_floor = false  # Last frame's on-floor state
 var jumping = false
@@ -13,12 +14,30 @@ const DECCELERATION = 4000
 func _ready() -> void:
 	pass
 func _physics_process(delta: float) -> void:
+
+	handle_jump(delta)
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Left", "Right")
+
+	flip_sprite(direction)
+	
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, ACCELERATION * delta)
+		
+	
+	move_and_slide()
+	
+# handles jump, gravity, and includes coyote time
+func handle_jump(delta):
 	if is_on_floor() and jumping:
 		jumping = false
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y +=  GRAVITY * delta
-	
 	
 	if last_floor and not is_on_floor() and not jumping:
 		coyote = true
@@ -31,23 +50,15 @@ func _physics_process(delta: float) -> void:
 		coyote = false
 	if not Input.is_action_pressed("Jump") and not is_on_floor():
 		velocity.y -= JUMP_VELOCITY/25
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left", "Right")
-	
+		
+	last_floor = is_on_floor()
+
+func flip_sprite(direction):
 	if direction != 0:
 		if direction == 1:
 			sprite.flip_h = false
 		else:
 			sprite.flip_h = true
-		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
-		#velocity = velocity.move_toward(SPEED, ACCELERATION * delta)
-	else:
-		velocity.x = move_toward(velocity.x, 0, ACCELERATION * delta)
-		last_floor = is_on_floor()
-	move_and_slide()
-
-
 
 func _on_coyote_timer_timeout() -> void:
 	coyote = false
