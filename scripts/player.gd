3213fcc0c82ddf685045e71a35e28399411301e5
Jasper-Cn <leshan.jasper@gmail.com>
2026-02-01 16:52:29 -0800

    Player hitbox remade

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,12 +12,12 @@ func _physics_process(delta: float) -> void:
 		velocity.y +=  GRAVITY * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
