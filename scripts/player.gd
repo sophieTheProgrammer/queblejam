3e7a4c88327f044e24d8540d92def3d00ba585c8
Jasper-Cn <leshan.jasper@gmail.com>
2026-02-01 17:09:47 -0800

    variable jump height

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,19 @@
 extends CharacterBody2D
 
-
-const SPEED = 600.0
-const JUMP_VELOCITY = -1000.0
-const GRAVITY = 2200
+const SPEED = 800.0
+const JUMP_VELOCITY = -1600.0
+const GRAVITY = 2900
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		#velocity += get_gravity() * delta
 		velocity.y +=  GRAVITY * delta
-
 	# Handle jump.
 	if Input.is_action_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	if not Input.is_action_pressed("Jump") and not is_on_floor():
+		velocity.y -= JUMP_VELOCITY/45
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
