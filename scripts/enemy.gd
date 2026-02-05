cdf9dfc589d4c741102082b0cd9fd25f22efc028
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2026-02-04 20:42:13 -0800

    working tutoiral!

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,24 +1,28 @@
 extends CharacterBody2D
-const movement_speed= 10000.0
+const movement_speed= 30000.0
 const RESET = preload("res://scenes/level_1.tscn")
 @export var Goal: Node = null
 @onready var navigation: NavigationAgent2D = $NavigationAgent2D
 @onready var timer: Timer = $Timer
 @export var activation: Area2D
-var activated : bool = false
+@export var activated : bool = true
 
 func _ready() -> void:
-
+	if activation:
+		activation.body_entered.connect(activate)
 	navigation.target_position = Goal.global_position
 
 func _physics_process (delta: float) -> void:
 	if !navigation.is_target_reached() and !Global.debug and activated: 
 		var nav_point_direction = to_local(navigation.get_next_path_position()).normalized()
 		velocity = nav_point_direction * movement_speed * delta
-		print(nav_point_direction)
 		move_and_slide()
+
 
 func _on_timer_timeout() -> void:
 	if navigation.target_position != Goal.global_position && !Global.debug:
 		navigation.target_position = Goal.global_position
 		timer.start()
+func activate(body):
+	print("wow")
+	activated = true
