e58f7d9b6cb6b6221e150bdca4a86ee7575312ad
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2026-02-04 18:41:44 -0800

    enemy in tutorial

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,20 +4,30 @@ const RESET = preload("res://scenes/level_1.tscn")
 @export var Goal: Node = null
 @onready var navigation: NavigationAgent2D = $NavigationAgent2D
 @onready var timer: Timer = $Timer
+@export var activation: Area2D
+var activated : bool = false
+
 func _ready() -> void:
+	if activation:
+		activation.body_entered.connect(activate)
 	navigation.target_position = Goal.global_position
 
 func _physics_process (delta: float) -> void:
-	if !navigation.is_target_reached() and !Global.debug:
+	if !navigation.is_target_reached() and !Global.debug and activated: 
 		var nav_point_direction = to_local(navigation.get_next_path_position()).normalized()
 		velocity = nav_point_direction * movement_speed * delta
+		print(nav_point_direction)
 		move_and_slide()
 
 func _on_timer_timeout() -> void:
 	if navigation.target_position != Goal.global_position && !Global.debug:
 		navigation.target_position = Goal.global_position
 		timer.start()
+		print("starting timer")
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if !Global.debug:
 		get_tree().reload_current_scene()
+
+func activate(body):
+	activated = true
