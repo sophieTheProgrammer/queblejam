facdd77e812ad8b8e686def74eb5947b01af9b62
Jasper-Cn <leshan.jasper@gmail.com>
2026-02-05 21:39:09 -0800

    level 3 is just a clone of level 2

--- scripts/death.gd
+++ scripts/death.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 func _on_area_entered(_area: Area2D) -> void:
 	if !Global.debug:
-		print("yipppe")
+		#print("yipppe")
 		SfxPlayer.play_sound(SfxPlayer.HURT, 10)
 		get_tree().call_deferred("reload_current_scene")
