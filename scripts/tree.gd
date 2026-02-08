4db35ce4c5f1c13a1c7cfe7c56d4d78514a7ac24
Jasper-Cn <leshan.jasper@gmail.com>
2026-02-07 19:54:03 -0800

    enemy rotation work, with move toward

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -3,7 +3,7 @@ extends AnimatableBody2D
 @onready var area: Area2D = $Area2D
 @onready var death_timer: Timer = $"Die Timer"
 var tree_dead := false
-const PLAY_ANIMATION = 1
+var PLAY_ANIMATION = 1
 const TIME_TILL_DEATH = 2
 
 # Called when the node enters the scene tree for the first time.
@@ -11,14 +11,16 @@ func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if tree_dead == true:
-		PLAY_ANIMATION
+		PLAY_ANIMATION = PLAY_ANIMATION
 func _on_area_2d_area_entered(_area: Area2D) -> void:
-	death_timer.start(TIME_TILL_DEATH)
+	if death_timer.is_stopped():
+		death_timer.start(TIME_TILL_DEATH)
+		
 
 func _on_die_timer_timeout() -> void:
-	print("a")
+	#print("a")
 	tree_dead = true
 	collision.disabled = true
 	area.monitoring = false
