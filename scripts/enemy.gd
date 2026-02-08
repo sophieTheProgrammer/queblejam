f4573e2262587ffd4781c5c8dc170b95807223f2
Jasper-Cn <leshan.jasper@gmail.com>
2026-02-07 20:03:25 -0800

    enemy rotation works now

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -14,11 +14,11 @@ func _ready() -> void:
 func _physics_process (delta: float) -> void:
 	if !navigation.is_target_reached() and !Global.debug and activated: 
 		position = position.move_toward(Goal.global_position, movement_speed * 100 * delta)
-		
+		look_at(Goal.global_position)
+		self.rotate(-PI/2)
 func _on_timer_timeout() -> void:
 	if navigation.target_position != Goal.global_position && !Global.debug and activated:
 		navigation.target_position = Goal.global_position
-		look_at(Goal.global_position)
 	timer.start()
 
 func activate(_body):
