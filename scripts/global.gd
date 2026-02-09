952197517aaf72b3265fa42758e336e42cca7259
Jasper-Cn <leshan.jasper@gmail.com>
2026-02-08 16:50:48 -0800

    gooooodod

--- scripts/global.gd
+++ scripts/global.gd
@@ -10,7 +10,7 @@ const LEVEL_4 = preload("res://scenes/level_4.tscn")
 const LEVEL_5 = preload("res://scenes/level_5.tscn")
 const START = preload("res://scenes/start.tscn")
 const RESTART = preload("res://scenes/restart.tscn")
-const LEVELS = [START, TUTORIAL_SCREEN, LEVEL_0, LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, RESTART]
+const LEVELS = [START, TUTORIAL_SCREEN, LEVEL_0, LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, LEVEL_5, RESTART]
 @export var Destination := 1
 
 func load_scene(scene:int):
