7f81b76450698eed5599c5d088e27ec3fe518692
Sophie <sophie.thi.nguyen11@gmail.com>
2026-03-09 17:43:46 -0700

    graphics are now amozing

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -3,6 +3,8 @@ extends AnimatableBody2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
 @onready var area: Area2D = $Area2D
 @onready var death_timer: Timer = $"Die Timer"
+
+
 var tree_dead := false
 const TIME_TILL_DEATH = 2
 
@@ -20,6 +22,7 @@ func _process(_delta: float) -> void:
 			sprite.hide()
 			collision.disabled = true
 			area.monitoring = false
+			$ColorRect.hide()
 func _on_area_2d_area_entered(_area: Area2D) -> void:
 	if death_timer.is_stopped():
 		death_timer.start(TIME_TILL_DEATH)
